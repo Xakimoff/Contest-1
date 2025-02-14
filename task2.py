234b48c6d1939f42ec146a2d45270ef0e1ef7d32
Xakimoff <147584600+Xakimoff@users.noreply.github.com>
2025-02-14 20:24:13 +0500

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,1 +1,18 @@
-
+returnedkwargs = {}
+    for key, value in kwargs.items():
+        if isinstance(value, (int, float)):
+            returnedkwargs[key] = value ** 2
+        elif isinstance(value, str):
+            returnedkwargs[key] = value[::-1]
+        elif isinstance(value, bool)
+            returnedkwargs[key] = not (value)
+        elif isinstance(value, list)
+            returnedkwargs[key] = value[::-1]
+        elif isinstance(value, tuple)
+            returnedkwargs[key] = value[::-1]
+        elif isinstance(value, dict)
+            returnedkwargs[key] = {v: k for k, v in value.items()}
+        else:
+            returnedkwargs[key] = value
+    
+    return returnedkwargs
