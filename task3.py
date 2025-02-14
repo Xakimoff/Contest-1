e04ae4ff471b72ebcd6276ef4f3caf46ce9b3659
Xakimoff <147584600+Xakimoff@users.noreply.github.com>
2025-02-14 20:41:48 +0500

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,1 +1,10 @@
+import time
 
+def decorator_1(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        exec_time = end_time - start_time
+        return result
+    return wrapper
